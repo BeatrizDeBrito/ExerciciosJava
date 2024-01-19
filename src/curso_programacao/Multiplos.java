b742e1e783e980488099126a009cd86f0582b5ac
BeatrizDeBrito <beatriz.de.brito@hotmail.com>
2024-01-19 16:27:27 -0300

    Verifica se é múltiplo ou não

---
+++ src/curso_programacao/Multiplos.java
@@ -0,0 +1,33 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class Multiplos {
+    public static void main(String[] args) {
+
+        int A, B;
+
+        Scanner scanner = new Scanner(System.in);
+
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+
+        scanner.close();
+
+        if (A > B){
+            if( A % B == 0){
+                System.out.println("Sao Multiplos");
+            } else{
+                System.out.println("Nao sao Multiplos");
+            }
+        }
+        if (B > A){
+            if( B % A == 0){
+                System.out.println("Sao Multiplos");
+            } else{
+                System.out.println("Nao sao Multiplos");
+            }
+        }
+
+    }
+}
