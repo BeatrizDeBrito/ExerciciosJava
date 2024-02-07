92a73a620e116f61377f83306089d379e806ae57
BeatrizDeBrito <beatriz.de.brito@hotmail.com>
2024-02-07 19:40:26 -0300

    Valor do desconto

---
+++ src/curso_programacao/ValorDoDesconto.java
@@ -0,0 +1,16 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class ValorDoDesconto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double preco = 34.5;
+        double desconto =  (preco < 20.0) ? preco * 0.1 :  preco *0.05;
+        System.out.println("Valor desconto: " + desconto);
+        scanner.close();
+
+
+    }
+}
