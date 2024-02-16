8153212152315720c9d2ea24d576aba7b5f3ae58
BeatrizDeBrito <beatriz.de.brito@hotmail.com>
2024-02-16 16:25:09 -0300

    Estrutura de repetição (for).

---
+++ src/curso_programacao/EstruturaFor.java
@@ -0,0 +1,22 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class EstruturaFor {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        int soma = 0;
+        for(int i = 0; i < N; i++){
+            int x = scanner.nextInt();
+            soma = soma + x;
+        }
+
+        System.out.println(soma);
+
+        scanner.close();
+    }
+}
