67c6a5d269076a11f4eba36c7c3ee61cec7874af
Beatriz de Brito <77403137+BeatrizDeBrito@users.noreply.github.com>
2024-08-30 15:10:37 -0300

    Torna classe estática

--- src/curso_programacao/CalculaCircunferenciaEVolume.java
+++ src/curso_programacao/CalculaCircunferenciaEVolume.java
@@ -5,22 +5,21 @@ import util.Calculator;
 import java.util.Locale;
 import java.util.Scanner;
 
-import static util.Calculator.*;
 
 public class CalculaCircunferenciaEVolume {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Calculator calc = new Calculator();
+
         System.out.println("Enter radius");
         double radius = sc.nextDouble();
 
-        double c = calc.circumference(radius);
-        double v = calc.volume(radius);
+        double c = Calculator.circumference(radius);
+        double v = Calculator.volume(radius);
 
         System.out.println("Circumference = " + c);
         System.out.println("Volume = " + v);
-        System.out.println(calc.PI);
+        System.out.println(Calculator.PI);
 
         sc.close();
     }
